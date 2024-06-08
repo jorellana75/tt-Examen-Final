77204628fb2c98b2d22563ef097f789b2979d957
jorellana75 <162061174+jorellana75@users.noreply.github.com>
2024-06-08 15:21:11 -0600

    Update tt_um_top_joms.v

--- src/tt_um_top_joms.v
+++ src/tt_um_top_joms.v
@@ -47,6 +47,25 @@ module tt_um_top_joms(
     assign uo_out[6]=1'b0;
     assign uo_out[7]=1'b0;
 
+    assign uio_out[0]=1'b0;
+	assign uio_out[1]=1'b0;
+	assign uio_out[2]=1'b0;
+	assign uio_out[3]=1'b0;
+	assign uio_out[4]=1'b0;
+	assign uio_out[5]=1'b0;
+	assign uio_out[6]=1'b0;
+	assign uio_out[7]=1'b0;
+    
+    assign uio_oe[0]=1'b0;
+	assign uio_oe[1]=1'b0;
+	assign uio_oe[2]=1'b0;
+	assign uio_oe[3]=1'b0;
+	assign uio_oe[4]=1'b0;
+	assign uio_oe[5]=1'b0;
+	assign uio_oe[6]=1'b0;
+	assign uio_oe[7]=1'b0;
+    
+
     //reg [7:0] ui_in;
     //reg [7:0] uio_in;
     //wire [7:0] uo_out;
