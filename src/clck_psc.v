36a748eb4ba5905507c9b2ca9a93f054ef209f56
jorellana75 <162061174+jorellana75@users.noreply.github.com>
2024-06-07 21:28:30 -0600

    Update clck_psc.v

--- src/clck_psc.v
+++ src/clck_psc.v
@@ -1,14 +1,17 @@
 `timescale 1ns / 1ps
-    
-//endmodule
 
-module clck_psc(
-    input CLK100MHZ,
-    output LED0
-    );
-    reg [13:0] myreg;  // Contador de 14 bits
+module clck_psc (
+    input wire CLK100MHZ,
+    output reg LED0
+);
+    reg [13:0] contador;  // Contador de 14 bits
+
+    // Siempre aumenta el contador en cada flanco de subida del reloj
     always @(posedge CLK100MHZ) begin
-        myreg <= myreg + 1;
+        contador <= contador + 1;
     end
-    assign LED0 = myreg[13];  // Divisor para obtener 10 kHz
+
+    // LED0 se activa cada 10 kHz (100 MHz / 10000)
+    assign LED0 = contador[13];  // Divisor para obtener 10 kHz
 endmodule
+
